bccd4565aac9f6b53d8bf579f8806a117091a126
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-02-05 15:11:28 -0800

    update test

--- test.sh
+++ test.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 export BEHAVE_FRONTEND=noninteractive
+
+cat <<EOF > config.properties
+users.admin.type=Parent
+EOF
+
 cat <<EOF | java -classpath out/production/behave-app com.company.Main config.properties
-login bob
-status
+login admin
 add-child alice
 add-child john
 status
